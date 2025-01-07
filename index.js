edae67deb6dc49fdf4a4a1485f7bf2ea1fbfd772
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-07 10:50:28 +0530

    bug fixed

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import DBcon from './libs/db.js';
 import AuthRouter from './routes/Auth.routes.js';
-
+import cors from 'cors';
 
 dotenv.config();
 DBcon();
 const PORT=process.env.PORT;
 const app=express();
 app.use(express.json())
-
+app.use(cors())
 
 app.use('/auth',AuthRouter)
 
