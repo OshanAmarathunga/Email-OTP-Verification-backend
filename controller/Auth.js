2ec9afb611407b0e1edc273a3d0f0ac187ad4e76
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-02-15 19:00:48 +0530

    add comment

--- controller/Auth.js
+++ controller/Auth.js
@@ -47,7 +47,7 @@ export const verifyEmail = async (req, res) => {
       email: email,
     });
 
-    if (!user) {
+    if (!user) { // check user validity
       return res.status(400).json({
         message: "Invalid user email",
       });
